d744d09c6587e2e87d39849744dc84291ea0a09e
Yanone <post@yanone.de>
2017-03-08 19:06:29 +0200

    Initial commit

---
+++ Space Bar 30-day Trial.glyphsReporter/Contents/Resources/plugin.py
@@ -0,0 +1,16 @@
+# encoding: utf-8
+
+###########################################################################################################
+#
+#
+#	Reporter Plugin
+#
+#	Read the docs:
+#	https://github.com/schriftgestalt/GlyphsSDK/tree/master/Python%20Templates/Reporter
+#
+#
+###########################################################################################################
+
+from spacinginvaderlib import SpacingInvader
+
+
